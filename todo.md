62e017d275a097329c705044efda8d35c2e6e02a
swayam <swayam1998@gmail.com>
2020-07-02 03:26:04 +0530

    Tweetme2 Clone Lec-50

---
+++ todo.md
@@ -0,0 +1,7 @@
+1. Tweets
+    ->Creating
+        -> Text
+        -> Image -> Media Storage Server
+    ->Delete
+    ->Retweeting
+
---
+++ tweets/models.py
@@ -0,0 +1,39 @@
+import random
+from django.conf import settings
+from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+# Create your models here.
+class Tweet(models.Model):
+    parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True  )
+
+    class Meta:
+        ordering = ['-id']
+
+    # def __str__(self):
+    #     return self.content
+
+    @property
+    def is_retweet(self):
+        return self.parent != None
+            
+    def serialize(self):
+        '''
+        Not used anymore
+        '''
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0,200),
+        }
